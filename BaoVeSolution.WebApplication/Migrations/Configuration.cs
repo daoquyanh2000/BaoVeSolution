b916480c46dbecabd3c2fc6ae56476e41ab994b4
teu <daoquyanh2000@gmail.com>
2022-08-26 08:11:38 +0700

    last final

--- BaoVeSolution.WebApplication/Migrations/Configuration.cs
+++ BaoVeSolution.WebApplication/Migrations/Configuration.cs
@@ -30,9 +30,7 @@ namespace Model.Migrations
                 consectetur adipiscing elit, sed do eiusmod tempor incididunt ut l
                 abore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
                 exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
-                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat null
-                a pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deser
-                unt mollit anim id est laborum.",
+                ",
                 OpenTime = "Tất cả các ngày",
                 CloseTime = "Trừ thứ 7 và cn",
             };
