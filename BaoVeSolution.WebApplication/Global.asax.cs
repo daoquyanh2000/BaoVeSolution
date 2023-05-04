7e3cc69d03276d00d964f45756e2712604755d57
Quy Anh Dao <daoquyanh2000@gmail.com>
2023-05-04 16:18:14 +0700

    Thêm số lượng người đang truy cập

--- BaoVeSolution.WebApplication/Global.asax.cs
+++ BaoVeSolution.WebApplication/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
@@ -12,6 +13,13 @@ namespace BaoVeSolution.WebApplication
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Application["Totaluser"] = 0;
+        }
+        protected void Session_Start()
+        {
+            Application.Lock();
+            Application["Totaluser"] = (int)Application["Totaluser"] + 1;
+            Application.UnLock();
         }
     }
 }
